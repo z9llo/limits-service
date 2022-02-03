f3d11cdbdfa5f49779a75b0658e6a7a9f3bf0b56
Gabriel <gabriel.conceicao@tivit.com>
2022-02-03 18:53:20 -0300

    Initial commit

---
+++ src/main/java/com/zllo/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,15 @@
+package com.zllo.microservices.limitsservice.controller;
+
+import com.zllo.microservices.limitsservice.bean.Limits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(1, 100);
+    }
+
+}
