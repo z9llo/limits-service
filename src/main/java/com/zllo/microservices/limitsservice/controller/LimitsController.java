803871c58f6824c0b54d5563924faf103dfd1bcd
Gabriel <gabriel.conceicao@tivit.com>
2022-02-05 17:20:36 -0300

    Consuming config server

--- src/main/java/com/zllo/microservices/limitsservice/controller/LimitsController.java
+++ src/main/java/com/zllo/microservices/limitsservice/controller/LimitsController.java
@@ -1,15 +1,24 @@
 package com.zllo.microservices.limitsservice.controller;
 
 import com.zllo.microservices.limitsservice.bean.Limits;
+import com.zllo.microservices.limitsservice.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    private Config config;
+
+    @Autowired
+    public LimitsController(Config config) {
+        this.config = config;
+    }
+
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return new Limits(1, 100);
+        return new Limits(this.config.getMinimum(), this.config.getMaximum());
     }
 
 }
